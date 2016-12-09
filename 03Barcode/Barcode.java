78154a1aefa9f167f52038453d84939f28899d5f
Patrick Chan <patrickchan1999@gmail.com>
2016-12-09 05:53:09 -0500

    Added toSum

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -64,10 +64,57 @@ public class Barcode implements Comparable<Barcode>{
 	    default:
 	    }
 	}
+	s = _zip + _checkDigit + " " + s;
 	return s;
     }
 
     public int compareTo(Barcode other){
 	return (Integer.valueOf(_zip + _checkDigit)).compareTo(Integer.valueOf(other._zip + other._checkDigit));
+    }
+
+    public static String toCode(String zip){
+	int sum = 0;
+	for (int num = Integer.parseInt(zip); num > 0; num = num / 10){
+	    sum = sum + num % 10;
+	}
+	int checkDigit = sum % 10;
+	String s = "|";
+	for (int barcode = Integer.parseInt(zip + checkDigit); barcode > 0; barcode = barcode / 10){
+	    int digit = barcode % 10;
+	    switch(digit){
+	    case 0:
+		s = "||:::" + s;
+		break;
+	    case 1:
+		s = ":::||" + s;
+		break;
+	    case 2:
+		s = "::|:|" + s;
+		break;
+	    case 3:
+		s = "::||:" + s;
+		break;
+	    case 4:
+		s = ":|::|" + s;
+		break;
+	    case 5:
+		s = ":|:|:" + s;
+		break;
+	    case 6:
+		s = ":||::" + s;
+		break;
+	    case 7:
+		s = "|:::|" + s;
+		break;
+	    case 8:
+		s = "|::|:" + s;
+		break;
+	    case 9:
+		s = "|:|::" + s;
+		break;
+	    default:
+	    }
+	}
+	return s;
     }
 }
