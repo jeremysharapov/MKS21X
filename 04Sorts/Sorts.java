c232e94f20ed6db5951831f5c31077148bcb8c4e
Jeremy Sharapov <jsharapov@stuy.edu>
2016-12-13 18:29:58 -0500

    Added Sorts, not quite sure what exactly is happening here because I was out, but I will catch up when I have time

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,13 @@
+public class Sorts{
+    public static String name(){
+	return "09.Sharapov.Jeremy";
+    }
+
+    /**Selection sort of an int array.
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data the elements to be sorted.
+     */
+
+    public static void selectionSort(int[] data){
+    }
+}
