b039258677c35b5727654107f8b6a6ba0e032818
Jeremy Sharapov <jsharapov@stuy.edu>
2016-12-16 06:02:54 -0500

    Created Bubble Sorts

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Sorts{
     public static String name(){
 	return "09.Sharapov.Jeremy";
@@ -13,10 +14,29 @@ public class Sorts{
 
     public static void insertSort(int[] data){
 	int inTransit;
-	for (int i = 1; i < data.length(); i++){
+	for (int i = 1; i < data.length; i++){
 	    inTransit = data[i];
-	    for (int n = i - 1; n >= 0; n--){
-		
+	    for (int n = i - 1; data[n] < data[i]; n--){
+		data[n + 1] = data[n];
+		data[n + 1] = inTransit;
+	    }
+	}
+    }
+
+    public static void bubbleSort(int[] data){
+	int didSwap = 1;
+	while (didSwap != 0){
+	    for (int i = 1; i < data.length; i++){
+		didSwap = 0;
+		if (data[i] < data[i - 1]){
+		    int store = data[i - 1];
+		    data[i - 1] = data[i];
+		    data[i] = store;
+		    didSwap = didSwap + 1;
+		}
+	    }
+	}
+    }
 
     
 }
